23e6a1653e3c090f6e7fd635539a6a61b92602cb
emp1tsu <oneok0813@gmail.com>
2021-08-23 19:32:00 +0900

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
